b2685161b90b2d69df7012efc6333e906cba069d
Chaitanya Kulkarni <chaitanyakulkarni050988@gmail.com>
2024-06-21 23:28:06 +0100

    Initial commit

---
+++ src/main/java/com/demo/jwtsecurity/controller/TokenController.java
@@ -0,0 +1,27 @@
+package com.demo.jwtsecurity.controller;
+
+import com.demo.jwtsecurity.model.JwtUser;
+import com.demo.jwtsecurity.security.JwtGenerator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
